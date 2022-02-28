d51cc4feac9e262ec839b9fc035e45aa56bf0789
jacoco11 <79934741+jacoco11@users.noreply.github.com>
2022-02-27 22:26:23 -0500

    jace edited

---
+++ TVWS_main.py
@@ -0,0 +1,148 @@
+import os
+import TVWS_tools
+
+# Globals
+directory = None
+graph = None
+filters = []
+totalCalcs = {}
+avgCalcs = {}
+
+
+# Function to set or update path of current working directory
+def set_directory():
+    global directory
+    loop2 = True
+    while loop2:
+        inpt = input("Enter directory: ")
+        if inpt == "exit": loop2 = False
+        elif inpt == "list": print(directory)
+        elif inpt == "info":
+            print("Set_Directory Info: " +
+                  "\nInput: " +
+                  "\n- 'list' : Outputs path to current working directory" +
+                  "\n- 'exit' : Returns to main menu")
+        elif os.path.isdir(inpt):
+            directory = inpt
+            print("Directory path set. ")
+            loop2 = False
+        else: print("Directory does not exist, re-renter. ")
+
+
+# Function to set or update fields to be filtered when processing .pcap files
+def set_filters():
+    global filters
+    loop2 = True
+    while loop2:
+        fltrs = list(input("Enter filters: ").split())
+        if fltrs[0] == "exit": loop2 = False
+        elif fltrs[0] == "info":
+            print("Set_Filter Info: " +
+                  "\nPreface command using: " +
+                  "\n- 'add' : Adds one or more fields to existing filter, separating arguments with a space" +
+                  "\n- 'remove' : Removes one or more fields from existing filter, separating arguments with a space" +
+                  "\n- 'clear' : Clears filter" +
+                  "\n- 'list' : Outputs current fields in filter" +
+                  "\n- 'exit' : Returns to main menu" +
+                  "\nNone of the above arguments clears filter and adds in one or more fields, separating with a space")
+        elif fltrs[0] == "add":
+            for item in fltrs:
+                if item!= "icmp.type" and item not in filters and item != "add":
+                    filters.append(item)
+            print("Filter(s) added. ")
+        elif fltrs[0] == "remove":
+            for item in fltrs:
+                if item != "remove" and item in filters:
+                    filters.remove(item)
+            print("Filter(s) removed. ")
+        elif fltrs[0] == "clear":
+            filters.clear()
+            print("Filters cleared. ")
+        elif fltrs[0] == "list":
+            print(filters)
+        else:
+            filters.clear()
+            for item in fltrs:
+                if item != "icmp.type": filters.append(item)
+            print("Filter(s) added. ")
+
+
+# Function to set graph type
+def set_graph():
+    global graph
+    loop2 = True
+    while loop2:
+        choice = input("Select graph type: ")
+        if choice == "exit":
+            loop2 = False
+        elif choice == "info":
+            print("Process Info: " +
+                  "\nInput: " +
+                  "\n- 'plot' : Set graph to plot" +
+                  "\n- 'scatter' : Set graph to scatter" +
+                  "\n- 'bar' : Set graph to bar")
+        elif choice == "plot" or choice == "scatter" or choice == "bar":
+            graph = choice
+            print("Graph set.")
+        else: print("Invalid entry, re-renter. ")
+
+
+def main():
+    global directory
+    global graph
+    global filters
+    global tempTotal
+    global tempAvg
+    global totalCalcs
+    global avgCalcs
+
+    print("PCAP Trace Processing Tool")
+    loop = True
+
+    while loop:
+        print("Main Menu: ")
+        choice = input()
+
+        if choice == "info":
+            print("Main Menu Info: " +
+                  "\nInput: " +
+                  "\n- 'dir' : Enter specified directory from which .pcap files are processed" +
+                  "\n- 'filter' : Enter fields to filter .pcap files from specified directory" +
+                  "\n- 'graph' : Enter fields to be present in graph and type of graph" +
+                  "\n- 'process' : Processes .pcap files in specified directory with chosen fields, and outputs them" +
+                  " in desired graph" +
+                  "\n- 'exit' : Exits PCAP Trace Processing Tool")
+        elif choice == "dir":
+            set_directory()
+        elif choice == "filter":
+            set_filters()
+        elif choice == "graph":
+            set_graph()
+        elif choice == "process":
+            TVWS_tools.process(directory, filters)
+        elif choice == "tools":
+            loop2 = True
+            while loop2:
+                choice2 = input("Select tool: ")
+                if choice2 == "info":
+                    print("Tools Info: " +
+                          "\nInput: " +
+                          "\n- 'calc' : Calculates total and averages among values" +
+                          "\n- 'graph' : Plots desired data points")
+                elif choice2 == "calc":
+                    totalValue, total, avgValue, avg = TVWS_tools.calc(directory, graph, filters)
+                    totalCalcs[totalValue] = total
+                    avgCalcs[avgValue] = avg
+                    print("TOTAL: ", totalCalcs[totalValue])
+                    print("AVERAGE: ", avgCalcs[avgValue])
+                elif choice2 == "graph":
+                    TVWS_tools.graph(directory, graph, filters)
+                elif choice2 == "exit":
+                    loop2 = False
+                else: print("Invalid command, re-enter. ")
+        elif choice == "exit":
+            loop = False
+        else: print("Invalid command, re-enter. ")
+
+
+main()
