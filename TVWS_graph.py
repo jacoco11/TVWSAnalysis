6f2a76817654407de58bff62b9c775b9cf41aece
keenen678 <keenentopple@gmail.com>
2022-03-06 17:56:00 -0500

    added to graph.py

--- TVWS_graph.py
+++ TVWS_graph.py
@@ -1,7 +1,6 @@
-'''
 import os
 import csv
-import pyqtgraph as pg
+'''import pyqtgraph as pg'''
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -16,6 +15,8 @@ def graph(directory, graph, filters):
             value2 = input("Enter field 2: ")
             if value1 in filters and value2 in filters:
                 check2 = False
+            elif value1 == "exit" or value2 == "exit":
+                check2 = False
             else:
                 print("Invalid field(s), re-enter. ")
         indx1 = int(filters.index(value1))
@@ -28,8 +29,11 @@ def graph(directory, graph, filters):
             print()
 
         elif graph == "bar":
+            fig = plt.figure()
+            ax = fig.add_axes([0,0,1,1])
+            ax.bar(value1,value2)
+            plt.show()
             print()
 
     else:
         print("No graph type selected. ")
-'''
